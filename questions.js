83811faa18ede2b76f6bc9061bd07c4bb97a593d
LZ <laraezegart@gmail.com>
2020-12-08 20:20:19 -0800

    added style.css to format main.html

--- questions.js
+++ questions.js
@@ -24,58 +24,54 @@ const questions = [
         name: 'role',
         choices: ['Intern', 'Engineer', 'Manager']
     },
-    
+
 ];
 
 
 // function to initialize questions
 function init() {
     inquirer
-      .prompt(questions)
-      .then((response) => {
-        const role = response.role  
-        console.log(role)
-        if (role === "Intern") {
-            inquirer
-                .prompt(
-                    {
-                    type: 'input',
-                    message: 'If you are an Intern, enter your school.',
-                    name: 'school',
-                })
-                .then((internResponse) => {
-                    console.log(internResponse) 
-                })
-        } else if (role === "Engineer") {
-            inquirer
-                .prompt(
-                    {
-                        type: 'input',
-                        message: 'If you are an Engineer, enter your GitHub username.',
-                        name: 'github',
+        .prompt(questions)
+        .then((response) => {
+            const role = response.role
+            console.log(role)
+            if (role === "Intern") {
+                inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: 'If you are an Intern, enter your school.',
+                            name: 'school',
+                        })
+                    .then((internResponse) => {
+                        console.log(internResponse)
                     })
-                .then((engineerResponse) => {
-                    console.log(engineerResponse) 
-                })
-        } else if (role === "Manager") {
-            inquirer
-                .prompt(
-                    {
-                        type: 'input',
-                        message: 'If you are a Manager, enter your office umber.',
-                        name: 'office',
+            } else if (role === "Engineer") {
+                inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: 'If you are an Engineer, enter your GitHub username.',
+                            name: 'github',
+                        })
+                    .then((engineerResponse) => {
+                        console.log(engineerResponse)
                     })
-                .then((managerResponse) => {
-                    console.log(managerResponse) 
-                })
-        }
-        // let storeInfo = render(response)
-        //   console.log(storeInfo)
-        //   console.log(response)
-      }
-  );
+            } else if (role === "Manager") {
+                inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: 'If you are a Manager, enter your office umber.',
+                            name: 'office',
+                        })
+                    .then((managerResponse) => {
+                        console.log(managerResponse)
+                    })
+            }
+        });
 }
- 
+
 // function call to initialize qs
 init();
 

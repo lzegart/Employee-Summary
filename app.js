3b886433bf18a5a1c51c45f39b1e24d7e597852b
LZ <laraezegart@gmail.com>
2020-12-12 17:51:05 -0800

    moved questions.js content to app.js to use node more efficiently

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
-const questions = require("./questions");
+// const questions = require("./questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -11,16 +11,138 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const team = require("team");
+// const team = require("team");
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the employee name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is the employee id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is the employee email?',
+        name: 'email',
+    },
+    {
+        type: 'list',
+        message: 'What is the employee role?',
+        name: 'role',
+        choices: ['Intern', 'Engineer', 'Manager']
+    },
+];
+
+// team will consist of employee objects
+const team = [];
+
+// function to initialize questions
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const role = response.role
+            console.log(role)
+            if (role === "Intern") {
+                inquirer
+                    .prompt(
+                        [{
+                            type: 'input',
+                            message: 'Enter intern school.',
+                            name: 'school',
+                        },
+                        {
+                            type: 'list',
+                            message: 'New employee?',
+                            name: 'new',
+                            choices: ['yes', 'no']
+                        },
+                    ])
+                    .then((internResponse) => {
+                        console.log(internResponse)
+                        let internEmployee = new Intern(response.name, response.id, response.email, internResponse.school)
+                        team.push(internEmployee)
+                        if(internResponse.new === 'yes') {
+                            init();
+                        } else {
+                            console.log(team)  
+                        }
+                    })
+            } else if (role === "Engineer") {
+                inquirer
+                    .prompt(
+                        [{
+                            type: 'input',
+                            message: 'Enter engineer GitHub username.',
+                            name: 'github',
+                        },
+                        {
+                            type: 'list',
+                            message: 'New employee?',
+                            name: 'new',
+                            choices: ['yes', 'no']
+                        },
+                    ])
+                    .then((engineerResponse) => {
+                        console.log(engineerResponse)
+                        let engineerEmployee = new Engineer(response.name, response.id, response.email, engineerResponse.github)
+                        team.push(engineerEmployee)
+                        if(engineerResponse.new === 'yes') {
+                            init();
+                        } else {
+                            console.log(team)  
+                        }
+                    })
+            } else if (role === "Manager") {
+                inquirer
+                    .prompt(
+                        [{
+                            type: 'input',
+                            message: 'Enter manager office number',
+                            name: 'office',
+                        },
+                        {
+                            type: 'list',
+                            message: 'New employee?',
+                            name: 'new',
+                            choices: ['yes', 'no']
+                        },
+                    ])
+                    .then((managerResponse) => {
+                        console.log(managerResponse)
+                        let managerEmployee = new Manager(response.name, response.id, response.email, managerResponse.office)
+                        team.push(managerEmployee)
+                        if(managerResponse.new === 'yes') {
+                            init();
+                        } else {
+                            console.log(team)  
+                        }
+                    })
+            } 
+        });
+}
+
+// function to initialize qs is called
+init();
 
 
+// function renderMain () {
+//     const HTML = render(team);
+//     fs.writeFile(outputPath, HTML, (err) => {
+//         if (err) throw err
+//     })
+// }
 
 const writeIt = () => {
     if (!fs.existsSync(OUTPUT_DIR)) {
       fs.mkdirSync(OUTPUT_DIR);
     }
   
-    fs.writeFile(outputPath, render(tean), (err) => {
+    fs.writeFile(outputPath, render(team), (err) => {
       if (err) throw err;
       console.log("Your file has been created in the output folder.")
     });

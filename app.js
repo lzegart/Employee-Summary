f0f15290c0c6e596e169fc4bc5fac6bc36e796b5
LZ <laraezegart@gmail.com>
2020-12-13 22:36:22 -0800

    removed provided comments and included other decriptive comments

--- app.js
+++ app.js
@@ -116,7 +116,8 @@ function init() {
                         if(managerResponse.new === 'yes') {
                             init();
                         } else {
-                            console.log(team)  
+                            console.log(team) 
+                            // function to render team and send to html is called 
                             createTeam();
                         }
                     })
@@ -127,7 +128,7 @@ function init() {
 // function to initialize qs is called
 init();
 
-
+// function to render team and write in an html file
 const createTeam = () => {
      if (!fs.existsSync(OUTPUT_DIR)) {
        fs.mkdirSync(OUTPUT_DIR);
@@ -138,24 +139,3 @@ const createTeam = () => {
        console.log("Success!")
      });
    };
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
